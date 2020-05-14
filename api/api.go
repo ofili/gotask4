d6022495961ad2cea38d97f3f05e0861ecf77358
Lewis <ofili@users.noreply.github.com>
2020-05-14 20:31:46 +0100

    commit

---
+++ api/api.go
@@ -0,0 +1,105 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+//Book represents the model for a book
+//Default table name will be `books`
+type Book struct {
+	//gorm.Model
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	PublishedAt string `json:"published_at"`
+}
+
+var books []Book
+
+//Initial DB setup and config
+var db *gorm.DB
+
+//Init
+func InitDB() {
+	var err error
+	db, err = gorm.Open("mysql", "root:+*P@ssw0rd)@tcp(localhost:3306)/library_db?parseTime=True")
+
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect database")
+	}
+	log.Println("Connection established")
+
+	//Creat the tables. This is a  one-time step
+	//Comment out if running multiple times - You may see an error otherwise
+	//db.CreateTable(&Book{})
+	db.HasTable(&Book{})
+	db.DropTableIfExists(&Book{})
+
+	//Migration to create tables for library_db schema
+	db.AutoMigrate(&Book{})
+}
+
+//Create api
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+	//Create new books by inserting records in the books table
+	db.Create(&book)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Must be a post request", http.StatusInternalServerError)
+		return
+	}
+	book = Book{
+		Name:        r.FormValue("name"),
+		Author:      r.FormValue("author"),
+		PublishedAt: time.Now().Local().String(),
+	}
+	books = append(books, book)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+
+//Read-all api
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	bk, err := json.Marshal(books)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bk)
+	//w.Header().Set("Content-Type", "application/json")
+	//json.NewEncoder(w).Encode(books)
+}
+
+//Read api
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := r.URL.Query()["id"]
+	db.Where("id = ?", param).Find(&books)
+	json.NewEncoder(w).Encode(books)
+}
+
+//Update api
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updatedBook Book
+	json.NewDecoder(r.Body).Decode(&updatedBook)
+	db.Save(&updatedBook)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedBook)
+}
+
+//Delete api
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := r.URL.Query()["id"]
+	db.Where("id = ?", param).Delete(&books)
+	json.NewEncoder(w).Encode(books)
+}
